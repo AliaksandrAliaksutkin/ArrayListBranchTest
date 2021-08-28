2d2d1916b2dfb1f0c805dc7ea19980cf710eca77
Aliaksutkin_Aliaksandr <aleksutkinaliaksandr@gmail.com>
2021-08-28 15:13:17 +0300

    ???

--- TestLinkedList.java
+++ TestLinkedList.java
@@ -5,7 +5,24 @@ import java.util.List;
 
 public class TestLinkedList {
     public static void main(String[] args) {
-        List <String> list = new LinkedList<>();
-        list.add("Джа все работает");
+        LinkedList <PersonMu> list = new LinkedList<>();
+
+        PersonMu person1 = new PersonMu("Джа");
+        PersonMu person2 = new PersonMu("Прапор");
+        PersonMu person3 = new PersonMu("Лазик");
+
+        list.add(person1);
+        list.add(person2);
+        list.add(person3);
+
+
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+
+    }
+    public static class PersonMu{
+
+        public PersonMu(String string) {
+        }
     }
 }
