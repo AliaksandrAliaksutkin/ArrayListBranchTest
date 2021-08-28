e51eea4022820b52d6f291addc25d77c33a526f3
Aliaksutkin_Aliaksandr <aleksutkinaliaksandr@gmail.com>
2021-08-28 11:43:54 +0300

    create secondBranch

---
+++ TestLinkedList.java
@@ -0,0 +1,11 @@
+package collection;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestLinkedList {
+    public static void main(String[] args) {
+        List <String> list = new LinkedList<>();
+        list.add("Джа все работает");
+    }
+}
