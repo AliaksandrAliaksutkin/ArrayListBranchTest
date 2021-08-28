98d536f893d1280e2b6633986b4d3f8fd5824b4d
Aliaksutkin_Aliaksandr <aleksutkinaliaksandr@gmail.com>
2021-08-28 22:25:55 +0300

    reTestLinkidList/addTeasIterator/addTestListIterator

--- TestLinkedList.java
+++ TestLinkedList.java
@@ -1,28 +1,19 @@
 package collection;
 
 import java.util.LinkedList;
-import java.util.List;
 
 public class TestLinkedList {
     public static void main(String[] args) {
-        LinkedList <PersonMu> list = new LinkedList<>();
+        LinkedList <String> list = new LinkedList<>();
 
-        PersonMu person1 = new PersonMu("Джа");
-        PersonMu person2 = new PersonMu("Прапор");
-        PersonMu person3 = new PersonMu("Лазик");
-
-        list.add(person1);
-        list.add(person2);
-        list.add(person3);
-
+        list.add("джа");
+        list.add("прапор");
+        list.add("Lazik");
 
         System.out.println(list.getFirst());
         System.out.println(list.getLast());
 
     }
-    public static class PersonMu{
 
-        public PersonMu(String string) {
-        }
     }
-}
+
